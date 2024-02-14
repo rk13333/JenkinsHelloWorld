25971f1892e7627dac691912dd24ba6475e21a7f
rk13333 <126143717+rk13333@users.noreply.github.com>
2024-02-14 18:57:46 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
  public static void main(String[] arg){
- System.out.println("----****Hellow world & welcome to jenkins training***---");
+ System.out.println("----**** Made Some Changes ***---");
  }
 }
